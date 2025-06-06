4ae7061348b15cf91131686177b0012bea794223
Milana <mkrestya@icloud.com>
2025-06-06 16:58:41 +0300

    Revert "Task01"
    This reverts commit 942ec41c32ad67adb4b702d25cacfa15b52d1ebe.

--- Task01/hello.php
+++
@@ -1,6 +0,0 @@
-<?php
-    date_default_timezone_set('Europe/Moscow');
-    $dateTime = date('Y-m-d H:i:s');
-    $name = 'Крестьянскова Милана';
-    echo "Текущая дата и время: $dateTime\n";
-    echo "Фамилия и имя: $name\n";
