8790ffcc33061925078c49197417d79f838a5fd9
Milana <mkrestya@icloud.com>
2025-06-06 16:59:36 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,6 @@
+<?php
+    date_default_timezone_set('Europe/Moscow');
+    $dateTime = date('Y-m-d H:i:s');
+    $name = 'Крестьянскова Милана';
+    echo "Текущая дата и время: $dateTime\n";
+    echo "Фамилия и имя: $name\n";
